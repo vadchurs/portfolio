a07d9a99385b6a56931248e61bdb05159baf4770
vadchurs90 <vadchurs90@gmail.com>
2022-07-19 21:12:29 +0300

    Updates

---
+++ precache-manifest.5bbe7531f9978e3919532878942848c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba0a5aa5e4135df9a4e09d7cae5346d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9fb081ce4e159f390d38",
+    "url": "/portfolio/static/css/main.a966157e.chunk.css"
+  },
+  {
+    "revision": "9ba85dd6261308fc9584",
+    "url": "/portfolio/static/js/2.573b2fbc.chunk.js"
+  },
+  {
+    "revision": "9fb081ce4e159f390d38",
+    "url": "/portfolio/static/js/main.c1b514c7.chunk.js"
+  },
+  {
+    "revision": "add6ff81d93680170b15",
+    "url": "/portfolio/static/js/runtime-main.2601f1e9.js"
+  },
+  {
+    "revision": "bed050013c7ee969f8e9d21c130a046a",
+    "url": "/portfolio/static/media/cropped-volcano-brandon-project.bed05001.jpg"
+  },
+  {
+    "revision": "3b9a5fbc8f954e9558e86e03eb4e1feb",
+    "url": "/portfolio/static/media/maxresdefault.3b9a5fbc.jpg"
+  },
+  {
+    "revision": "3e497f21d2b3cce8fdb2d14e35364805",
+    "url": "/portfolio/static/media/todo-list-850x476.3e497f21.jpg"
+  },
+  {
+    "revision": "cab05f6a618c9f4966a414ab631c3fc0",
+    "url": "/portfolio/static/media/vad.cab05f6a.jpg"
+  }
+]);
